611fe280259ce782db5ddef12a55acdfb7bcb2d1
Yongce Tu <yongce.tu@gmail.com>
2017-02-21 17:37:26 +0800

    Add demos

---
+++ mobile/src/main/java/me/ycdev/android/demo/androidwear/MainActivity.java
@@ -0,0 +1,13 @@
+package me.ycdev.android.demo.androidwear;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
